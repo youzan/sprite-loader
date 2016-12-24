63f9ffa0defb021f94917aec47d30ca959c2c5f0
liwei <liwei@youzan.com>
2016-12-24 17:51:37 +0800

    first commit

---
+++ test/index.js
@@ -0,0 +1,52 @@
+'use strict';
+const css = require('css');
+const assert = require('assert');
+const loader = require('../lib/loader');
+
+describe('sprite-loader', function () {
+    it('should getBgImgDef', function () {
+        let def1 = loader.getBgImgDef({declarations: [{property: 'background', value: 'url(../test.png)'}]});
+        let def2 = loader.getBgImgDef({declarations: [{property: 'background-image', value: 'url("../test.png")'}]});
+        let def3 = loader.getBgImgDef({
+            declarations: [{
+                property: 'background',
+                value: 'url( ../test.png) no-repeat top left'
+            }]
+        });
+        assert.equal(def1.url, '../test.png');
+        assert.equal(def2.url, '../test.png');
+        assert.equal(def3.url, '../test.png');
+    });
+
+    it('should getRepeatType', function () {
+        let val1 = loader.getRepeatType({
+            declarations: [{
+                property: 'background',
+                value: 'url(../test.png) repeat-x top left'
+            }]
+        });
+        let val2 = loader.getRepeatType({
+            declarations: [{
+                property: 'background-repeat',
+                value: 'repeat-y'
+            }]
+        });
+        assert.equal(val1, 'x');
+        assert.equal(val2, 'y');
+    });
+
+    it('should group', function () {
+        let cssText = '.class1 {background: url(http://a.cdn.cn/pic1.png) repeat-x;}\
+                    .class2 {background: url(http://a.cdn.cn/pic2.png) repeat-x;}\
+                    .class3 {background-image: url(http://a.cdn.cn/pic3@2x.png);}\
+                    .class4 {background: url(http://a.cdn.cn/pic4@2x.png);}\
+                    .class5 {background: url(http://a.cdn.cn/pic5@2x.png); background-position: top left}';
+        let ast = css.parse(cssText);
+        let groups = loader.grouping(ast.stylesheet.rules);
+        assert.equal(groups.length, 2);
+        assert.equal(groups[0].repeat, 'x');
+        assert.equal(groups[0].defs.length, 2);
+        assert.equal(groups[1].ratio, 2);
+        assert.equal(groups[1].defs.length, 2);
+    });
+});
